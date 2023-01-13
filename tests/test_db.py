8bd14e115caa63e822a1bfc48cfa68d76ff16ebf
rzv09 <rzv090701@gmail.com>
2023-01-12 22:07:18 -0500

    added test_init_db function

--- tests/test_db.py
+++ tests/test_db.py
@@ -13,4 +13,16 @@ def test_get_close_db(app):
     
     assert 'closed' in str(e.value)
 
+def test_init_db_command(runner, monkeypatch):
+    class Recorder(object):
+        called = False
+    
+    def fake_init_db():
+        Recorder.called = True
+    
+    monkeypatch.setattr('flask.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
+
     
